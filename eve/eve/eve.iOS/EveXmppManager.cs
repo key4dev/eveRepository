e1b988a49565d1cb3ff41a6fc8e0c78dd5fcc197
f.cuscino <f.cuscino@PORT1>
2017-01-23 17:59:46 +0100

    modifiche per connect

---
+++ eve/eve/eve.iOS/EveXmppManager.cs
@@ -0,0 +1,34 @@
+﻿using S22.Xmpp;
+using S22.Xmpp.Client;
+using S22.Xmpp.Extensions;
+using S22.Xmpp.Im;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System;
+
+  
+ 
+namespace eve
+{
+     
+
+    public class EveXmppManager
+    {
+         
+        public bool connect()
+        {
+            XmppClient xmppClient = new XmppClient("192.168.168.73", "test", "test");
+            xmppClient.Port = 5222;
+            xmppClient.Connect();
+            xmppClient.SetStatus(Availability.Online, null, 1);
+            if (xmppClient.Connected == true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
